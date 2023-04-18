78c714fb98cfc6dd1db389b143ad2a81bb06d380
NomadHuns <kimalss22@gmail.com>
2023-04-18 09:48:02 +0900

    Repository 테스트 코드 수정

---
+++ test/model/auth/user_repository_test.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter_riverpod_blog_start/dto/response_dto.dart';
+import 'package:flutter_riverpod_blog_start/dto/user_request.dart';
+import 'package:flutter_riverpod_blog_start/model/auth/user_repository.dart';
+import 'package:flutter_riverpod_blog_start/model/user/user.dart';
+import 'package:logger/logger.dart';
+
+void main() async {
+  await fetchJoin_test();
+}
+
+Future<void> fetchJoin_test() async{
+  JoinRequestDTO joinRequestDTO = JoinRequestDTO(username: "babo12", password: "1234", email: "babo1@naver.com");
+  ResponseDTO response = await UserRepository().fetchJoin(joinRequestDTO);
+  Logger().d(response.msg);
+  User user = response.data;
+  Logger().d(user.username);
+}
